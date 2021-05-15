c394f283bb9975ac740fe1eb8ab3b9186ca3769f
Marut <marut.dhungana@gmail.com>
2021-05-15 21:07:40 +0545

    Add project files.

---
+++ NumberList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   public class NumberList
+    {
+       
+        public void sumArray(int[] array)
+        {
+            int total = array.Sum();
+            Console.WriteLine("Sum of array is  " + total);
+            
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberList nmb = new NumberList();
+            int[] array = { 25, 85, 95, 87, 25, 87, 96, 25, 45 };
+            nmb.sumArray(array);
+            Console.ReadKey();
+        }
+    }
+}
